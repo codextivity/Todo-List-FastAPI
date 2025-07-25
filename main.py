8e6293051f8ab5a3ca9cdadc91d9d038e4a3eeae
codextivity <vandetpann@naver.com>
2025-07-25 09:47:29 +0900

    Added uvicorn

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI,Request
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 import json
+import uvicorn
 
 app=FastAPI()
 templates=Jinja2Templates(directory="templates")
@@ -36,3 +37,6 @@ async def add_todo(request:Request):
     with open('database.json','w') as f:
         json.dump(newdata,f)
     return RedirectResponse("/",303)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
